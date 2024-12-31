6074dd54fc247296eae4c5afaf68807ae93c3927
NeonGamerBot-QK <NeonGamerBot-QK@users.noreply.github.com>
2024-12-31 06:20:18 +0000

    Prettified Code!

--- docs/gallery/icon.js
+++ docs/gallery/icon.js
@@ -1,9 +1,9 @@
-function setupIcon(iconf, el ) {
-    const icon = document.createElement('img')
-    icon.src = "./icons/"+iconf+".png"
-    // icon.width = "20px"
-    // icon.height = "20px"
-    icon.style.width = "40px"
-    icon.style.height = "40px"
-    return el ? el.appendChild(icon) : icon
+function setupIcon(iconf, el) {
+  const icon = document.createElement("img");
+  icon.src = "./icons/" + iconf + ".png";
+  // icon.width = "20px"
+  // icon.height = "20px"
+  icon.style.width = "40px";
+  icon.style.height = "40px";
+  return el ? el.appendChild(icon) : icon;
 }
