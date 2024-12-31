baaaabc11b8cc85081555a68db6736e2f9a1949f
NeonGamerBot-QK <NeonGamerBot-QK@users.noreply.github.com>
2024-12-31 06:20:18 +0000

    Prettified Code!

--- docs/gallery/carousel.js
+++ docs/gallery/carousel.js
@@ -1,24 +1,24 @@
 function setupCarousel() {
-    let currentSlide = 0;
-const slides = document.querySelectorAll('.carousel img');
-const totalSlides = slides.length;
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".carousel img");
+  const totalSlides = slides.length;
 
-function moveSlide(direction) {
+  function moveSlide(direction) {
     currentSlide += direction;
-    
+
     if (currentSlide < 0) {
-        currentSlide = totalSlides - 1;
+      currentSlide = totalSlides - 1;
     } else if (currentSlide >= totalSlides) {
-        currentSlide = 0;
+      currentSlide = 0;
     }
 
     updateCarouselPosition();
-}
+  }
 
-function updateCarouselPosition() {
-    const carousel = document.querySelector('.carousel');
+  function updateCarouselPosition() {
+    const carousel = document.querySelector(".carousel");
     const offset = -currentSlide * 100;
     carousel.style.transform = `translateX(${offset}%)`;
-}
-return { moveSlide }
+  }
+  return { moveSlide };
 }
